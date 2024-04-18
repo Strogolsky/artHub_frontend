ea18247e9e1155d9cdb4dcd33374f54ad010f96c
Strogolsky <Strogolsky@gmail.com>
2024-04-18 10:22:48 +0200

    6: add: create page createFolder and EditFolder without style

---
+++ src/components/Folder/EditFolder.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function EditFolder() {
+    return (
+        <div>
+            <div>
+                <button>Back</button>
+            </div>
+            <div>
+                <h1>Edit Folder</h1>
+                <input></input>
+                <input></input>
+                <button>Delete</button>
+                <button>Edit</button>
+
+            </div>
+        </div>
+
+
+    );
+}
+
+export default EditFolder;
---
+++ src/components/Folder/createFolder.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function CreateFolder() {
+    return (
+        <div>
+            <div>
+                <button>Back</button>
+            </div>
+            <div>
+                <h1>CreateFolder</h1>
+                <input></input>
+                <input></input>
+                <button>Create</button>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default CreateFolder;
