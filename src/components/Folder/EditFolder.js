675dfae763b6fdf717468958fe6d7bcdc44fb213
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-04-18 23:02:18 +0200

    refactor #6: change quotes in classNames

--- src/components/Folder/EditFolder.js
+++ src/components/Folder/EditFolder.js
@@ -7,17 +7,17 @@ function EditFolder() {
         <div>
             <div>
                 <button
-                    className='m-3 bg-my-purple hover:bg-my-purple-light font-regular py-2 px-6 
-                    rounded-large text-base active:bg-my-purple-dark' style={{ fontSize: '16px' }}>Back</button>
+                    className="m-3 bg-my-purple hover:bg-my-purple-light font-regular py-2 px-6
+                    rounded-large text-base active:bg-my-purple-dark" style={{ fontSize: '16px' }}>Back</button>
             </div>
-            <div className='flex flex-col items-center justify-center h-screen'>
-                <h1 className='m-10 font-bold text-base' style={{ fontSize: '48px' }}>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <h1 className="m-10 font-bold text-base" style={{ fontSize: '48px' }}>
                     Edit Folder
                 </h1>
                 <div>
                     <input
                         type="text"
-                        className='m-2 bg-my-light-grey font-regular h-10 w-72 py-2 px-4 rounded-large focus:outline-my-purple-light'
+                        className="m-2 bg-my-light-grey font-regular h-10 w-72 py-2 px-4 rounded-large focus:outline-my-purple-light"
                         placeholder="Title"
                         value={folderName}
                         onChange={(e) => setFolderName(e.target.value)}
@@ -26,7 +26,7 @@ function EditFolder() {
                 </div>
                 <div>
                     <textarea
-                        className='m-4 bg-my-light-grey font-regular h-40 w-96 py-2 px-4 rounded-large resize-none focus:outline-my-purple-light'
+                        className="m-4 bg-my-light-grey font-regular h-40 w-96 py-2 px-4 rounded-large resize-none focus:outline-my-purple-light"
                         placeholder="Description"
                         value={folderDescription}
                         onChange={(e) => setFolderDescription(e.target.value)}
@@ -35,12 +35,12 @@ function EditFolder() {
                 </div>
                 <div className="flex">
                     <button
-                        className='my-1 mx-4 bg-red-500 hover:bg-red-400 active:bg-red-700 font-regular py-3 px-5 rounded-large text-base' style={{ fontSize: '24px' }}
+                        className="my-1 mx-4 bg-red-500 hover:bg-red-400 active:bg-red-700 font-regular py-3 px-5 rounded-large text-base" style={{ fontSize: '24px' }}
                     >
                         Delete
                     </button>
                     <button
-                        className='my-1 mx-4 bg-my-purple hover:bg-my-purple-light font-regular py-3 px-5 rounded-large text-base active:bg-my-purple-dark' style={{ fontSize: '24px' }}
+                        className="my-1 mx-4 bg-my-purple hover:bg-my-purple-light font-regular py-3 px-5 rounded-large text-base active:bg-my-purple-dark" style={{ fontSize: '24px' }}
                     >
                         Edit
                     </button>
