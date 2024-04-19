f70f18768a46104a2b31dcc8309527da6d1d8980
Strogolsky <Strogolsky@gmail.com>
2024-04-19 17:01:11 +0200

    feat: #8: create page CreatePost

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import CreateFolder from './components/Folder/CreateFolder';
 import EditFolder from './components/Folder/EditFolder';
 import EditPost from './components/Post/EditPost';
+import CreatePost from './components/Post/CreatePost';
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/post/create" element={<EditPost />} />
+          <Route path="/post/create" element={<CreatePost />} />
+          <Route path="/post/:postId/edit" element={<EditPost />} />
           <Route path="/folder/create" element={<CreateFolder />} />
           <Route path="/folder/:folderId/edit" element={<EditFolder />} />
         </Routes>
