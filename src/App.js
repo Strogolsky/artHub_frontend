89e619656864eb87bb1700635ef37efe0c70e5c5
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-04-19 21:48:05 +0200

    feat #11: add route for main page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import CreateFolder from './components/Folder/CreateFolder';
 import EditFolder from './components/Folder/EditFolder';
 import EditPost from './components/Post/EditPost';
 import CreatePost from './components/Post/CreatePost';
+import MainPage from "./components/Main/MainPage";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/post/create" element={<CreatePost />} />
-          <Route path="/post/:postId/edit" element={<EditPost />} />
-          <Route path="/folder/create" element={<CreateFolder />} />
-          <Route path="/folder/:folderId/edit" element={<EditFolder />} />
+          <Route path="/">
+            <Route index element={<MainPage />} />
+            <Route path="post/create" element={<CreatePost />} />
+            <Route path="post/:postId/edit" element={<EditPost />} />
+            <Route path="folder/create" element={<CreateFolder />} />
+            <Route path="folder/:folderId/edit" element={<EditFolder />} />
+          </Route>
         </Routes>
       </div>
     </Router>
