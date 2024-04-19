c06bee5bbd8a7fd2fb302038a5fe3ee3bd5758e4
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-04-19 16:32:28 +0200

    feat #7: change font in CreateFolder

--- src/components/Folder/CreateFolder.js
+++ src/components/Folder/CreateFolder.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 function CreateFolder() {
     const [folderName, setFolderName] = useState("");
@@ -7,39 +7,33 @@ function CreateFolder() {
     return (
         <div>
             <div>
-                <button
-                    className="m-3 bg-my-purple hover:bg-my-purple-light font-regular py-2 px-6
-                    rounded-large text-base active:bg-my-purple-dark" style={{ fontSize: '16px' }}>Back</button>
+                <button className="m-3 bg-my-purple hover:bg-my-purple-light py-2 px-6 rounded-large text-base active:bg-my-purple-dark"
+                        style={{fontSize: '16px'}}>
+                    Back
+                </button>
             </div>
             <div className="flex flex-col items-center justify-center h-screen mt-n10">
-                <h1
-                    className="m-10 font-bold text-base" style={{ fontSize: '48px' }}>
+                <h1 className="m-10 kanit-bold text-base" style={{fontSize: '48px'}}>
                     New folder
                 </h1>
                 <div>
-                    <input
-                        type="text"
-                        className="m-2 bg-my-light-grey font-regular h-10 w-72 py-2 px-4 rounded-large focus:outline-my-purple-light"
-                        placeholder="Title"
-                        value={folderName}
-                        onChange={(e) => setFolderName(e.target.value)}
-                        style={{ width: '400px', height: '40px' }}
-                    />
+                    <input type="text"
+                           className="m-2 bg-my-light-grey h-10 w-72 py-2 px-4 rounded-large focus:outline-my-purple-light"
+                           placeholder="Title"
+                           value={folderName}
+                           onChange={(e) => setFolderName(e.target.value)}
+                           style={{width: '400px', height: '40px'}}/>
                 </div>
                 <div>
-                    <textarea
-                        className="m-4 bg-my-light-grey font-regular h-40 w-96 py-2 px-4 rounded-large resize-none focus:outline-my-purple-light"
-                        placeholder="Description"
-                        value={folderDescription}
-                        onChange={(e) => setFolderDescription(e.target.value)}
-                        style={{ width: '400px' }}
-                    />
+                    <textarea className="m-4 bg-my-light-grey h-40 w-96 py-2 px-4 rounded-large resize-none focus:outline-my-purple-light"
+                              placeholder="Description"
+                              value={folderDescription}
+                              onChange={(e) => setFolderDescription(e.target.value)}
+                              style={{width: '400px'}}/>
                 </div>
 
-                <button
-                    className="m-1 bg-my-purple hover:bg-my-purple-light
-                    font-regular py-3 px-5 rounded-large text-base active:bg-my-purple-dark" style={{ fontSize: '24px' }}
-                >
+                <button className="m-1 bg-my-purple hover:bg-my-purple-light py-3 px-5 rounded-large text-base active:bg-my-purple-dark"
+                        style={{fontSize: '24px'}}>
                     Create
                 </button>
             </div>
