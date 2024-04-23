9d8c4066bb8e41eceaad0c2432450ecce832851d
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-04-23 13:48:25 +0200

    refactor #15: cross icon into another component

--- src/components/Main/SignIn.js
+++ src/components/Main/SignIn.js
@@ -1,5 +1,6 @@
 import {Button, Card, CardBody, CardFooter, Dialog, Input, Typography} from "@material-tailwind/react";
 import {useState} from "react";
+import CrossIcon from "../Icons/CrossIcon";
 
 const SignIn = ({isOpen, setIsOpen, swapOpen}) => {
     const [email, setEmail] = useState("");
@@ -14,9 +15,7 @@ const SignIn = ({isOpen, setIsOpen, swapOpen}) => {
                 <Card className="flex flex-col gap-4">
                     <CardBody>
                         <div className="flex flex-col">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="mr-3 h-5 w-5" onClick={handleIsOpen}>
-                                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd"/>
-                            </svg>
+                            <CrossIcon className="mr-3 h-5 w-5" onClick={handleIsOpen} />
                             <Typography className="kanit-bold flex justify-center mb-7" variant="h2" color="blue-gray">
                                 Sign In
                             </Typography>
