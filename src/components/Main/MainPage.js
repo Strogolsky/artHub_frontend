8f22a0a91a937c50cbea310793ed97a1dd27328a
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-04-26 16:52:44 +0200

    feat #45: implement fetch posts from api

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import { useNavigate } from "react-router-dom";
 
 
 const MainPage = () => {
+    const API = `http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`;
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${API}/api/posts`)
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                setPosts(data);
+            })
+    }, []);
+
+
     const navigate = useNavigate();
 
     const [searchText, setSearchText] = useState("");
@@ -17,33 +32,6 @@ const MainPage = () => {
         setIsSignUpOpen((curr) => !curr);
         setIsSignInOpen((curr) => !curr);
     }
-
-    const [posts, setPosts] = useState([
-        {
-            "id": 0,
-            "image": "https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80",
-        },
-        {
-            "id": 1,
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1200px-Cat_August_2010-4.jpg",
-        },
-        {
-            "id": 2,
-            "image": "https://st2.depositphotos.com/2001755/5408/i/450/depositphotos_54081723-stock-photo-beautiful-nature-landscape.jpg",
-        },
-        {
-            "id": 3,
-            "image": "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-        },
-        {
-            "id": 4,
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvFBa3G11OUBYADP7ouSBgwiiRzSYorF4dfg&usqp=CAU",
-        },
-        {
-            "id": 5,
-            "image": "https://media.wired.com/photos/5fb70f2ce7b75db783b7012c/master/pass/Gear-Photos-597589287.jpg",
-        }
-    ])
 
     return (
         <div>
@@ -88,16 +76,16 @@ const MainPage = () => {
                 <div className="text-center grid grid-cols-1 items-center justify-center md:grid-cols-2 lg:grid-cols-3">
 
                     {posts.map((post) => (
-                        <div className="m-6" style={{ width: '310px', height: '400px' }}>
+                        <div key={post.id} className="m-6" style={{ width: '310px', height: '400px' }}>
                             <div className="rounded-large flex justify-center items-center"
                                     style={{ width: '300px', height: '385px' }}>
                                 <img key={post.id}
                                     className="hover:border-my-purple hover:cursor-pointer hover:border-4 object-cover rounded-large"
-                                    alt="postName"
-                                    src={post.image}
+                                    alt={post.title}
+                                    src={post.image.data}
                                     onClick={() => navigate(`/post/${post.id}`)}/>
                             </div>
-                            <p className="m-1">postName</p>
+                            <p className="m-1">{post.title}</p>
                         </div>
                     ))}
 
