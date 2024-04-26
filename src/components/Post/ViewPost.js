5508a7f0e4b2c42c5f373948c825b91ca9e8825f
Strogolsky <Strogolsky@gmail.com>
2024-04-26 11:14:35 +0200

    feat #14: add modal window "Add to folder"

--- src/components/Post/ViewPost.js
+++ src/components/Post/ViewPost.js
@@ -1,31 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import SignIn from "../Main/SignIn";
 import SignUp from "../Main/SignUp";
+import AddToFolder from "../Folder/AddToFolder";
 
 const ViewPost = () => {
     const [searchText, setSearchText] = useState("");
     const [isAuthorised, setIsAuthorised] = useState(false);
     const [isSignUpOpen, setIsSignUpOpen] = useState(false);
     const [isSignInOpen, setIsSignInOpen] = useState(false);
+    const [isAddToFolderOpen, setAddToFolderOpen] = useState(false);
+
+    const toggleAddToFolder = () => setAddToFolderOpen(!isAddToFolderOpen);
 
     const swapOpen = () => {
-        setIsSignUpOpen((curr) => !curr);
-        setIsSignInOpen((curr) => !curr);
+        setIsSignUpOpen((curr) => !curr)
+        setIsSignInOpen((curr) => !curr)
     }
 
     const imageUrl = "https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80";
-    const imageAutor = "https://images.unsplash.com/photo-1576174464184-fb78fe882bfd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    const imageAutor = "https://images.unsplash.com/photo-1576174464184-fb78fe882bfd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     const postTitle = "My Awesome Post";
     const postAuthor = "Post Author";
-    const imageDescription = "Lorem ipsum dolor sit amet consectetur. Donec faucibus non nibh id eleifend. Porttitor facilisis ull\
-    amcorper aenean tempus hac. Pulvinar velit vulputate lorem elementum. Et vulputate in eleifend sit ut aliquam.";
+    const imageDescription = "Lorem ipsum dolor sit amet consectetur. Donec faucibus non nibh id eleifend. Porttitor facilisis ullamcorper aenean tempus hac. Pulvinar velit vulputate lorem elementum. Et vulputate in eleifend sit ut aliquam.";
     const postTags = ["Tag1", "Tag2", "Mountain"];
 
     return (
         <div>
             <div className="flex justify-between items-center">
                 <div className="pl-2 flex-none">
-                    <img alt="ArtHub logo" src="path-to-your-logo.jpg" />
+                    <img alt="ArtHub logo" />
                 </div>
 
                 <div className="flex-grow text-center mx-auto">
@@ -48,34 +51,27 @@ const ViewPost = () => {
                     </div>
                 ) : (
                     <div className="pr-2 flex">
+
                         <div className="mr-5">
                             <SignUp isOpen={isSignUpOpen} setIsOpen={setIsSignUpOpen} swapOpen={swapOpen} />
                         </div>
+
                         <SignIn isOpen={isSignInOpen} setIsOpen={setIsSignInOpen} swapOpen={swapOpen} />
                     </div>
                 )}
-            </div>
 
+
+            </div>
             <div className="flex justify-center items-center h-screen">
                 <div className="w-1/2 flex justify-end">
-                    <img
-                        src={imageUrl}
-                        className="object-cover rounded-large"
-                        alt="Selected Post"
-                        style={{ maxWidth: '90%', maxHeight: '85vh' }}
-                    />
+                    <img src={imageUrl} className="object-cover rounded-large" alt="Selected Post" style={{ maxWidth: '90%', maxHeight: '85vh' }} />
                 </div>
                 <div className="w-1/2 flex justify-start flex-col items-left">
                     <div className="mx-12">
-                        <div >
+                        <div className="flex items-center space-x-4">
+                            <AddToFolder isOpen={isAddToFolderOpen} onClose={toggleAddToFolder} />
                             <button
-                                className="my-1 bg-my-purple hover:bg-my-purple-light font-regular py-3 px-5 rounded-large text-base active:bg-my-purple-dark"
-                                style={{ fontSize: '16px' }}
-                            >
-                                Save
-                            </button>
-                            <button
-                                className="my-1 mx-4 bg-my-purple hover:bg-my-purple-light font-regular py-3 px-5 rounded-large text-base active:bg-my-purple-dark"
+                                className="bg-my-purple hover:bg-my-purple-light font-regular py-3 px-5 rounded-large text-base active:bg-my-purple-dark"
                                 style={{ fontSize: '16px' }}
                             >
                                 Edit
@@ -92,14 +88,12 @@ const ViewPost = () => {
                                 </p>
                             </button>
                             <div className="flex-shrink-0 h-12 w-12 rounded-full overflow-hidden ml-2">
-                                <button className="w-full h-full object-cover bg-my-purple" />
+                                <button className="w-full h-full object-cover bg-my-purple hover:bg-my-purple-light active:bg-my-purple-dark" />
                             </div>
                         </div>
-
                         <div className="my-4 rounded-large bg-my-light-grey" style={{ width: '380px', height: '195px' }}>
                             <p className="p-2 kanit" style={{ fontSize: '12px' }}>{imageDescription}</p>
                         </div>
-
                         <div className="my-4 rounded-large bg-my-light-grey" style={{ width: '380px', height: '195px' }}>
                             <div className="rounded-large flex flex-wrap overflow-auto pb-3" style={{ maxHeight: '380px' }}>
                                 {postTags.map((tag, idx) =>
@@ -111,11 +105,10 @@ const ViewPost = () => {
                                 )}
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
 
