b742afe31f63a1ebc8f092deb08c46c5457e3cf5
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-05-09 11:53:56 +0200

    feat #56: add update folder by id API

--- src/api/FolderAPI.js
+++ src/api/FolderAPI.js
@@ -36,4 +36,36 @@ const getFolderById = async(folderId) => {
     return await response.json();
 }
 
-export { getUserFolders, getFolderById };
+const updateFolderById = async (folderId, folderData) => {
+    const url = `${FOLDER_URL}/${folderId}`;
+    const jwt = Cookies.get('jwt');
+
+    if (!jwt)
+        throw new Error("403 Forbidden");
+
+    const posts = folderData.posts ? folderData.posts.join(',') : '';
+
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${jwt}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: folderData.title,
+            description: folderData.description,
+            postIds: folderData.posts
+        })
+    });
+
+    if (!response.ok) {
+        if (response.status === 403) {
+            throw new Error("403 Forbidden");
+        }
+        throw new Error("505 Internal Server Error");
+    }
+
+    return await response.json();
+}
+
+export { getUserFolders, getFolderById, updateFolderById };
