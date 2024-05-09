03921be77b5acc5bbeb56a55a2e0b9321f6c2d73
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-05-09 11:54:19 +0200

    feat #56: implement updating folder

--- src/components/Folder/EditFolder.js
+++ src/components/Folder/EditFolder.js
@@ -1,11 +1,56 @@
-import {useState} from 'react';
-import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+import {getFolderById, updateFolderById} from "../../api/FolderAPI";
+import Loading from "../Loading";
+import NotFound from "../NotFound";
 
 function EditFolder() {
     const navigate = useNavigate();
 
-    const [folderName, setFolderName] = useState("");
-    const [folderDescription, setFolderDescription] = useState("");
+    const { folderId } = useParams();
+
+    const [folderData, setFolderData] = useState({
+        title: '',
+        description: '',
+        posts: []
+    });
+
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        getFolderById(folderId)
+            .then((data) => {
+                setFolderData({
+                    title: data.title || '',
+                    description: data.description || '',
+                    posts: data.posts.map((post) => post.id) || []
+                })
+            })
+            .catch((error) => {
+                setIsError(true);
+                console.log("Error getting folder: ", error);
+            })
+    }, [folderId]);
+
+    if (isError) return <NotFound />
+    if (!folderData) return <Loading />
+
+    const handleChange = (e) => {
+        setFolderData({
+            ...folderData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleEdit = async () => {
+        try {
+            const updatedFolder = await updateFolderById(folderId, folderData);
+            console.log("Successfully updated folder");
+            navigate(`/folder/${folderId}`);
+        } catch (error) {
+            console.log("Failed to update folder: ", error);
+        }
+    }
 
     return (
         <div>
@@ -24,15 +69,17 @@ function EditFolder() {
                     <input type="text"
                            className="m-2 bg-my-light-grey h-10 w-72 py-2 px-4 rounded-large focus:outline-my-purple-light"
                            placeholder="Title"
-                           value={folderName}
-                           onChange={(e) => setFolderName(e.target.value)}
+                           name="title"
+                           value={folderData.title}
+                           onChange={handleChange}
                            style={{ width: '400px', height: '40px' }}/>
                 </div>
                 <div>
                     <textarea className="m-4 bg-my-light-grey h-40 w-96 py-2 px-4 rounded-large resize-none focus:outline-my-purple-light"
                               placeholder="Description"
-                              value={folderDescription}
-                              onChange={(e) => setFolderDescription(e.target.value)}
+                              name="description"
+                              value={folderData.description}
+                              onChange={handleChange}
                               style={{width: '400px'}}/>
                 </div>
                 <div className="flex">
@@ -40,7 +87,8 @@ function EditFolder() {
                         Delete
                     </button>
                     <button className="my-1 mx-4 bg-my-purple hover:bg-my-purple-light py-3 px-5 rounded-large text-base active:bg-my-purple-dark"
-                            style={{fontSize: '24px'}}>
+                            style={{fontSize: '24px'}}
+                            onClick={handleEdit}>
                         Edit
                     </button>
                 </div>
