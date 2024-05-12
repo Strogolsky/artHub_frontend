1d4527c34702001af04e2b81af56890b3429fbb0
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-05-12 16:01:26 +0200

    refactor #18: incapsulate sign up API

--- src/api/AuthAPI.js
+++ src/api/AuthAPI.js
@@ -1,7 +1,7 @@
 const AUTH_URL = `http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/api/auth`
 
 const signIn = async (credentials) => {
-    const url = `${AUTH_URL}/signin`;
+    const url = `${AUTH_URL}/login`;
 
     const response = await fetch(url, {
         method: 'POST',
@@ -18,5 +18,23 @@ const signIn = async (credentials) => {
     return await response.json();
 }
 
+const signUp = async (credentials) => {
+    const url = `${AUTH_URL}/signup`;
 
-export { signIn };
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: credentials
+    })
+
+    if (!response.ok) {
+        throw new Error("500 Internal Server Error");
+    }
+
+    return await response.json();
+}
+
+
+export { signIn, signUp };
