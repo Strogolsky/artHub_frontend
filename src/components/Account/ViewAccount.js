653df9e73da1c74389da6450ce9310afc66ae711
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-05-16 16:08:22 +0200

    refactor #51: add authorisation component

--- src/components/Account/ViewAccount.js
+++ src/components/Account/ViewAccount.js
@@ -5,6 +5,7 @@ import NotFound from "../NotFound";
 import {getUserFolders} from "../../api/FolderAPI";
 import Loading from "../Loading";
 import SearchInput from "../Search/SearchInput";
+import Authorisation from "../Authorisation";
 
 const ViewAccount = () => {
     const navigate = useNavigate();
@@ -41,11 +42,7 @@ const ViewAccount = () => {
 
                 <SearchInput />
 
-                <div className="mr-5 relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                    <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
-                    </svg>
-                </div>
+                <Authorisation />
             </div>
 
             <div className="container flex flex-col items-center justify-center mx-auto mt-10">
--- src/components/Search/SearchPage.js
+++ src/components/Search/SearchPage.js
@@ -5,6 +5,7 @@ import NotFound from "../NotFound";
 import SearchInput from "./SearchInput";
 import SignUp from "../Main/SignUp";
 import SignIn from "../Main/SignIn";
+import Authorisation from "../Authorisation";
 
 
 const SearchPage = () => {
@@ -47,22 +48,7 @@ const SearchPage = () => {
 
                 <SearchInput initSearchText={searchParam}/>
 
-                {isAuthorised ? (
-                    <div className="mr-3 relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                        <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
-                        </svg>
-                    </div>
-                ) : (
-                    <div className="pr-2 flex">
-
-                        <div className="mr-5">
-                            <SignUp isOpen={isSignUpOpen} setIsOpen={setIsSignUpOpen} swapOpen={swapOpen} setIsAuthorised={setIsAuthorised}/>
-                        </div>
-
-                        <SignIn isOpen={isSignInOpen} setIsOpen={setIsSignInOpen} swapOpen={swapOpen} setIsAuthorised={setIsAuthorised}/>
-                    </div>
-                )}
+                <Authorisation />
             </div>
 
             <div className="mt-14 mb-10 flex justify-center">
